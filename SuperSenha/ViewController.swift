4a2ddbbb6dc2206aed01b9f780f0501c22cc2dbe
Vinícius Tinajero Salomão <viniciustsalomao@hotmail.com>
2020-08-20 20:27:54 -0300

    Initial Commit

---
+++ SuperSenha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperSenha
+//
+//  Created by Vinícius Tinajero Salomão on 20/08/20.
+//  Copyright © 2020 Vinícius Tinajero Salomão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
