24dab15f4da7f9f809e41969ffe4945ef0ddea16
Vinícius Tinajero Salomão <viniciustsalomao@hotmail.com>
2020-08-20 22:51:20 -0300

    Finalização do app

---
+++ SuperSenha/SecurityTipsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecurityTipsViewController.swift
+//  SuperSenha
+//
+//  Created by Vinícius Tinajero Salomão on 20/08/20.
+//  Copyright © 2020 Vinícius Tinajero Salomão. All rights reserved.
+//
+
+import UIKit
+
+class SecurityTipsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
--- SuperSenha/ViewController.swift
+++ SuperSenha/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfTotalPasswords: UITextField!
+    @IBOutlet weak var tfNumberOfCharacters: UITextField!
+    @IBOutlet weak var swLetters: UISwitch!
+    @IBOutlet weak var swNumbers: UISwitch!
+    @IBOutlet weak var swCapitalLetters: UISwitch!
+    @IBOutlet weak var swSpecialCharacters: UISwitch!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let passwordsViewController = segue.destination as! PasswordsViewController
+        
+        if let numberOfPasswords = Int(tfTotalPasswords.text!) {
+            passwordsViewController.numberOfPasswords = numberOfPasswords
+        }
+        
+        if let numberOfCharacters = Int(tfNumberOfCharacters.text!) {
+            passwordsViewController.numberOfCharacters = numberOfCharacters
+        }
+        
+        passwordsViewController.useLetters = swLetters.isOn
+        passwordsViewController.useNumbers = swNumbers.isOn
+        passwordsViewController.useCapitalLetters = swCapitalLetters.isOn
+        passwordsViewController.useSpecialCharacters = swSpecialCharacters.isOn
+        
+        view.endEditing(true)
+
+    }
 
 }
 
